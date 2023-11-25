f7aa7397ac3cbb3ef0d6bf283734372c6e3de34f
owlJunDev <anime_i_game@mail.ru>
2023-11-25 07:46:17 +0000

    uplaod last updata

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="d-main">
+        <div class="d-up">d-up</div>
+
+        <div class="d-left">d-left</div>
+        
+        <div class="d-right">d-right</div>
+    </div>
+</template>
+<script></script>
+<style>
+div {
+    text-align: center;
+    color:aliceblue
+}
+
+.d-main {
+    border: 5px solid #f00;
+    background-color: rgb(37, 37, 39);
+    padding: 0;
+}
+.d-main div{
+    border: 1px solid rgb(126, 235, 245);
+}
+
+
+.d-left {
+    width: 40%;
+    display: inline;
+}
+
+.d-right {
+    width: 40%;
+    display: inline;
+}
+</style>
